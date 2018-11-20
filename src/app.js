64e6d08518f98a216c8f75989a52a7d53c485ede
thiag <html5ehp@gmail.com>
2018-11-20 16:33:37 -0200

    separando as rotas,criando diretorio routes

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
 const express = require('express')
 const app = express()
-const router = express.Router()
-const bodyParser = require('body-parser')
-const controller = require('../src/controllers/product-controller')
+const router = require('./routes/product-route')
+const indexRoute = require('./routes/index-route')
 
 router.use((req, res, next) => {
     console.log(`Url: ${req.url}, Metodo:${req.method}, Time: ${Date.now()}`);
     next();
 })
 
-router.get('/',controller.get)
-router.post('/products', controller.post)
-router.put('/products/:id', controller.put)
-router.delete('/products', controller.deleta)
-
 app.use(express.json())
-app.use('/',router)
+app.use('/',indexRoute)
+app.use('/products',router)
 // app.use('/products', create)
 // app.use('/products', put)
 // app.use('/products', del)
---
+++ src/routes/product-route.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/product-controller')
+
+router.use((req, res, next) => {
+    console.log(`Url: ${req.url}, Metodo:${req.method}, Time: ${Date.now()}`);
+    next();
+})
+
+router.post('/', controller.post)
+router.put('/:id', controller.put)
+router.delete('/', controller.deleta)
+
+module.exports = router
